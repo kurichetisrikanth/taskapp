1ba19fea6e54540518e7f8f296b90148f942f38b
srikanth <srikanth.kuricheti@kony.com>
2020-06-01 18:25:22 +0700

    Updates

---
+++ precache-manifest.1939a95c68226d324bbbf88a59f36492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38a4b958eafdfcec5273f0504a4710b",
+    "url": "/taskapp/index.html"
+  },
+  {
+    "revision": "925a495139aaa9f3709c",
+    "url": "/taskapp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cf8da5f1c015feefa1e2",
+    "url": "/taskapp/static/css/main.ed554b74.chunk.css"
+  },
+  {
+    "revision": "925a495139aaa9f3709c",
+    "url": "/taskapp/static/js/2.7e8ab9e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taskapp/static/js/2.7e8ab9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8da5f1c015feefa1e2",
+    "url": "/taskapp/static/js/main.28265cd5.chunk.js"
+  },
+  {
+    "revision": "bc267c1402c626af473e",
+    "url": "/taskapp/static/js/runtime-main.5e9747bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taskapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
